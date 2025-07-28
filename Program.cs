f9c14bf43320be3bb596205d3a677f0ac97ef3dd
cjsonnnnn <cjsonnnnn@gmail.com>
2025-07-29 03:41:58 +0700

    [DEV] wuhuu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Jason! It's working!");
+app.MapGet("/", () => "Hello World! Jason! It's working! WUHUUU!!!");
 
 app.Run();

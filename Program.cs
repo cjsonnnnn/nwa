e0f800846ee1330b7290304bbc076a3bdadc7b18
cjsonnnnn <cjsonnnnn@gmail.com>
2025-08-07 20:44:51 +0700

    [DEV] demo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://+:80");
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! Jason! It's working! WIHIII!!!");
+app.MapGet("/", () => "Cihuyyy, icikiwir => Mima suka Jason sekali, selamanya. 🥰😤");
 
 app.Run();
